d514ad1c719348b3d49ff86584f38525a23f8f01
grace <graceokereke22@gmail.com>
2020-09-24 02:37:16 +1400

    changes

--- random.js
+++
@@ -1,8 +0,0 @@
-
-// let result = document.getElementById("result");
-// let resultScore = document.getElementById("resultScore");
-            // firstIcon.style.display = "none";
-            // secondIcon.style.display = "none";
-            // thirdIcon.style.display = "block";
-            // resultScore.innerHTML = 0;
-            // document.getElementById("Round 1").style.display = "none";
